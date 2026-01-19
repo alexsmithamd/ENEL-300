962a149f624e4bdfaca5f065df654dd6fb9e45c3
Zabro000 <reinzabrocki@shaw.ca>
2026-01-19 08:12:31 -0700

    Made sub dir and file for any python/matlab calculations. Added a .venv locally

---
+++ power_board/calculations/calculations.py
@@ -0,0 +1,27 @@
+import math as m
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+
+def save_results(name_list, number_list):
+    df = pd.DataFrame({'name' : name_list, 'values': number_list})
+    df.to_csv('calculation_results.csv', index = False)
+
+
+def function():
+    x = np.arange(100)
+    y = np.atan(x)
+    
+def test():
+    function()
+    arr = np.array([1,2,3,4])
+    names = ["a", 'b', 'c', 'd']
+    save_results(names, arr)
+
+
+def main():
+    test()
+
+
+if __name__ == "__main__":
+    main()
