f8f5b699a523577ecd836486bdf305f63acd5df0
Zabro000 <reinzabrocki@shaw.ca>
2026-01-19 22:34:24 -0700

    made component fucntions py file

--- power_board/calculations/calculations.py
+++ power_board/calculations/calculations.py
@@ -2,6 +2,7 @@ import math as m
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
+import component_functions as cf
 
 def save_results(name_list, number_list):
     df = pd.DataFrame({'name' : name_list, 'values': number_list})
