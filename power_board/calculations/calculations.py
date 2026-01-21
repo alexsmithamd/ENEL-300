94e68be75705faa9e916a8c6744e9ae66985e595
Zabro000 <reinzabrocki@shaw.ca>
2026-01-20 18:32:15 -0700

    got the auto save of calculations working

--- power_board/calculations/calculations.py
+++ power_board/calculations/calculations.py
@@ -3,11 +3,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
 import component_functions as cf
+from datetime import datetime
+import os
 
 def save_results(name_list, number_list):
-    df = pd.DataFrame({'name' : name_list, 'values': number_list})
-    df.to_csv('calculation_results.csv', index = False)
+    now = datetime.now()
+    time = now.strftime("%Y_%m_%d,%H_%M_%S")
 
+    name = f"{time} calculation_results.csv"
+    path = os.getcwd() + "/calculations"
+
+    df = pd.DataFrame({'name' : name_list, 'values': number_list})
+    df.to_csv(f"{path}/{name}", index = False)
 
 def function():
     x = np.arange(100)
@@ -26,3 +33,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    print(2 + 4 + 7 + 1 + 1 + 1 + 9 + 4 + 5 + 1 + 3 + 1 + 8 + 7 + 8 + 2 + 1)
